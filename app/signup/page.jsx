5bc3587eb51d920c9ac00f0245763740e44b2b30
Prabuddha Chatterjee <prabuddhapiku@gmail.com>
2023-09-20 23:59:45 +0530

    Signup Tailwind fix

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -42,12 +42,12 @@ const page = () => {
   if (user) redirect("/");
   return (
     <div className="w-full h-screen py-5 bg-[#0b1539] text-stone-200">
-      <div className="w-1/4 h-full rounded-3xl bg-gradient-to-b from-[#ea64dc] to-[#2d3e7a] mx-auto items-center">
-        <div className="p-2 m-auto w-full text-center text-4xl font-space-mono">
+      <div className="w-1/3 h-full rounded-3xl bg-gradient-to-b from-[#ea64dc] to-[#0b1539] mx-auto items-center">
+        <div className="pt-7 pb-3 m-auto w-full  text-center font-bold text-5xl text-white font-space-mono">
           Sign Up
         </div>
-        <div className="p-3 items-center flex flex-col  m-auto w-full  font-space-mono">
-          <div className="flex flex-col gap-4">
+        <div className="pt-5 items-center flex flex-col  m-auto w-full  font-space-mono">
+          <div className="pb-2 flex flex-col gap-1">
             <span className="text-2xl">Name</span>
             <input
               className="w-[19rem]  h-10 rounded-xl p-3 text-sm bg-[#0b1539] text-stone-200 "
@@ -56,8 +56,8 @@ const page = () => {
             />
           </div>
         </div>
-        <div className="p-3 items-center flex flex-col  m-auto w-full font-space-mono">
-          <div className="flex flex-col gap-4">
+        <div className="p-1 items-center flex flex-col  m-auto w-full font-space-mono">
+          <div className="pb-2 flex flex-col gap-1">
             <span className="text-2xl">Email</span>
             <input
               className="w-[19rem] h-10 text-sm rounded-xl p-3 bg-[#0b1539] text-stone-200"
@@ -66,31 +66,31 @@ const page = () => {
             />
           </div>
         </div>
-        <div className="p-3 items-center flex flex-col  m-auto w-full text-2xl font-space-mono">
-          <div className="flex flex-col gap-4">
+        <div className="p-1 items-center flex flex-col  m-auto w-full text-2xl font-space-mono">
+          <div className="pb-5 flex flex-col gap-1">
             Password
             <input
-              className="w-full h-10 rounded-xl p-3 bg-[#0b1539] text-stone-200"
+              className="w-[19rem] h-10 rounded-xl p-3 bg-[#0b1539] text-stone-200"
               type="password"
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
         </div>
-        <div className="text-center m-auto text-2xl bg-[#cc5ac6] w-fit h-9  rounded-full px-2">
-          <span className="p-4 text-center" onClick={handleEmailSignUp}>
+        <div className="w-36 h-custom m-auto text-center pt-2 text-2xl font-bold bg-[#cc5ac6] rounded-full">
+          <span className="p-6 text-center" onClick={handleEmailSignUp}>
             Create
           </span>
         </div>
-        <div className="text-center p-3">OR</div>
+        <div className="text-center text-xs pt-6">OR</div>
         <div className="p-1 items-center flex flex-col  m-auto w-full text-2xl font-space-mono">
           <div className="flex gap-6">
             <FcGoogle
               className="cursor-pointer"
-              size={50}
+              size={35}
               onClick={handleGoogleSignIn}
             />
             <BsGithub
-              size={50}
+              size={35}
               onClick={handleGitSignIn}
               className="cursor-pointer"
             />
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      borderRadius: {
+        '3xl': '3.5rem', // Adjust the value as needed
+      },
+      height: {
+        'custom': '3rem', // Define a custom height value
+      },
     },
   },
   plugins: [],
