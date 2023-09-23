bf0b76365a739b9e79b3de057c873e3589c37e76
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-09-23 21:15:13 +0530

    Login

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -8,35 +8,31 @@ import {
   Box,
 } from "@mui/material";
 import Link from "next/link";
-
+import { UserAuth } from "../context/AuthContext";
 const Projects = ({ projects }) => {
   console.log(projects);
+  const { user } = UserAuth();
   return (
     <div style={{ overflow: "hidden", padding: "20px" }}>
       <Stack flexWrap="wrap" justifyContent="start" gap={5} direction="row">
         {projects.map((item, index) => (
           <Box key={index}>
-            <Link href={`/project/${item.projectID}`}>
+            <Link href={user ? `/project/${item.projectID}` : "/signin"}>
               <Card className="h-auto transform transition duration-150 ease-in rounded-[1.5rem] hover:scale-105">
                 <CardMedia
                   component="img"
                   alt={item.title}
                   src={item.thumbnailUrl}
                   sx={{
-                    width: 250, 
+                    width: 250,
                     height: 112.5, // 16:9 aspect ratio (9 / 16 = 0.5625)
                   }}
                 />
                 <CardContent className="bg-[#0b1539] w-auto h-10 flex items-center justify-center transform transition duration-150 ease-in">
-                  <Typography
-                    variant="h6"
-                    component="div"
-                    color="#ffffff"
-                  >
+                  <Typography variant="h6" component="div" color="#ffffff">
                     {item.title}
                   </Typography>
                 </CardContent>
-
               </Card>
             </Link>
           </Box>
