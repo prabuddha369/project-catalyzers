cc7249f0291a59ccd3d1019bdac016c916f91946
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-09-23 21:32:56 +0530

    divided div

--- app/project/[id]/page.jsx
+++ app/project/[id]/page.jsx
@@ -35,16 +35,17 @@ export default function Page({ params }) {
         });
     }
   }, [project]);
-  
+
   console.log(OwnerName);
   console.log(project);
   return (
     <div className="px-20 h-full w-full bg-[#0b1539]">
       <div className="flex justify-between place-items-center">
-        <div className=" py-4 text-white flex gap-8 text-xl">
-          <GiHamburgerMenu size={40} />{OwnerName}
+        <div className=" py-4 text-white flex gap-8 text-xl place-items-center">
+          <GiHamburgerMenu size={40} />
+          {OwnerName}
         </div>
-        <div className="flex justify-between gap-4 text-white p-4">
+        <div className="flex justify-between gap-4 text-white p-4 place-items-center">
           <AiFillMail size={30} />
           <Link href="/">
             <BiHomeAlt size={30} />
@@ -52,22 +53,21 @@ export default function Page({ params }) {
         </div>
       </div>
       <div className="m-auto w-[100%] h-screen bg-gradient-to-b from-[#ea64dc] to-[#0b1539] rounded-2xl">
-        {project ? (
-          <div>
-            <div className="flex justify-center items-center">
+        <div className="flex justify-between">
+          <div className="w-1/2">
+            <div className="flex flex-wrap justify-center items-center">
               <div className="text-5xl text-white font-space-mono font-bold">
                 {project[0]?.title}
               </div>
             </div>
-            <div className="flex justify-center items-center">
-              <div className="text-2xl text-white font-space-mono font-bold">
+            <div className="flex flex-wrap justify-center items-center">
+              <div className="text-md text-white font-space-mono font-bold">
                 {project[0]?.description}
               </div>
             </div>
           </div>
-        ) : (
-          ""
-        )}
+          <div className="w-1/2"></div>
+        </div>
       </div>
     </div>
   );
