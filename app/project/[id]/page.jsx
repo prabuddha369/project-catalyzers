e4c3aca811f331e0a50ce57dc90e6d0ec3ced6bb
Prabuddha Chatterjee <prabuddhapiku@gmail.com>
2023-09-24 16:49:20 +0530

    Temp

--- app/project/[id]/page.jsx
+++ app/project/[id]/page.jsx
@@ -3,17 +3,24 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiFillMail } from "react-icons/ai";
 import { BiHomeAlt } from "react-icons/bi";
+import Image from "next/image";
 import Hierarchy from "../../hierarchy/hierarchy";
 import {
   GetProjectData,
   GetUserName,
+  GetUserPhotoUrl
 } from "../../utils/GetData.js";
+import { convertEmailToDomain } from "../../utils/UpdateData";
+import { UserAuth } from "../../context/AuthContext";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 export default function Page({ params }) {
+  
+  const {user} = UserAuth();
   const [project, setProject] = useState([]);
+  const [userDp, setUserDp] = useState("");
   const [OwnerName, setOwnerName] = useState("");
-  const [url, setUrl] = useState("");
+  const [ownerdpurl, setOwnerdpurl] = useState("");
   const projectID = params.id;
   useEffect(() => {
     // Fetch project data
@@ -40,32 +47,51 @@ export default function Page({ params }) {
         });
     }
   }, [project]);
+
   useEffect(() => {
     // Fetch owner's name asynchronously and update state
-    if (project[0]?.yturl) {
-      convertYouTubeURLToEmbed(project[0]?.yturl)
+    if (project[0]?.owner) {
+      GetUserPhotoUrl(project[0]?.owner)
         .then((link) => {
-          setUrl(link);
+          setOwnerdpurl(link);
         })
         .catch((error) => {
           // Handle errors if needed
           console.error(error);
         });
     }
-  }, [url]);
+  }, [project[0]?.owner]);
+  useEffect(() => {
+    // Fetch owner's name asynchronously and update state
+    if (user) {
+      GetUserPhotoUrl(convertEmailToDomain(user.email))
+        .then((link) => {
+          setUserDp(link);
+        })
+        .catch((error) => {
+          // Handle errors if needed
+          console.error(error);
+        });
+    }
+  }, [user]);
+
   const customStoragePath = projectID + "/";
-  console.log(project[0]?.yturl);
-  console.log(OwnerName);
-  console.log(project);
+  //console.log(project[0]?.yturl);
+  //console.log(OwnerName);
+  //console.log(project);
+  console.log(ownerdpurl);
+  console.log(userDp);
 
   return (
     <div className="h-full w-full bg-[#0b1539]">
       <div className="flex  justify-between  bg-[#0b1539] sticky top-0 w-full shadow-md shadow-black">
         <div className="text-white flex gap-8 text-xl place-items-center ps-10">
           <GiHamburgerMenu size={40} />
+          <Image src={ownerdpurl} alt="photo" height={50} width={50} className="rounded-full" />
           {OwnerName}
         </div>
-        <div className="flex justify-between gap-4 text-white p-4 pe-10">
+        <div className="flex justify-between gap-4 text-white p-4 pe-10 place-items-center">
+          <Image src={userDp} alt="Current User Photo" height={50} width={40} className="rounded-full" />
           <AiFillMail size={30} />
           <Link href="/">
             <BiHomeAlt size={30} />
