f95d5ed45270fe19fe8a3e154c6dfa60c1ed7613
Prabuddha Chatterjee <prabuddhapiku@gmail.com>
2023-10-14 11:59:53 +0530

    Message design fix

--- app/message/page.jsx
+++ app/message/page.jsx
@@ -1,10 +1,88 @@
 "use client";
-import React, { useState } from 'react';
-import { UserAuth } from "../context/AuthContext";
 import { ChatEngine, getOrCreateChat } from 'react-chat-engine';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross1 } from "react-icons/rx";
+import { BiHomeAlt } from "react-icons/bi";
+import Image from "next/image";
+import {
+	GetUserName,
+	GetUserPhotoUrl,
+	GetFollower,
+	GetFollowing,
+} from "../utils/GetData.js";
+import { convertEmailToDomain } from "../utils/UpdateData";
+import { UserAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Message = () => {
-	const { user } = UserAuth();
+	const { user, logOut } = UserAuth();
+	const [userDp, setUserDp] = useState("");
+	const [UserName, setUserName] = useState("");
+	const [Followers, setFollowers] = useState(0);
+	const [Following, setFollowing] = useState(0);
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+	const toggleDropdown = () => {
+		setIsDropdownOpen(!isDropdownOpen);
+	};
+	const handleSignOut = async () => {
+		try {
+			await logOut();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		// Fetch owner's name asynchronously and update state
+		if (user) {
+			GetUserPhotoUrl(convertEmailToDomain(user.email))
+				.then((link) => {
+					setUserDp(link);
+				})
+				.catch((error) => {
+					// Handle errors if needed
+					console.error(error);
+				});
+		}
+	}, [user]);
+
+	useEffect(() => {
+		// Fetch owner's name asynchronously and update state
+		if (user) {
+			GetUserName(convertEmailToDomain(user.email))
+				.then((name) => {
+					setUserName(name);
+				})
+				.catch((error) => {
+					// Handle errors if needed
+					console.error(error);
+				});
+		}
+	}, [user]);
+
+	useEffect(() => {
+		// Fetch owner's name asynchronously and update state
+		if (user) {
+			GetFollower(convertEmailToDomain(user.email))
+				.then((count) => {
+					setFollowers(count);
+				})
+				.catch((error) => {
+					// Handle errors if needed
+					console.error(error);
+				});
+			GetFollowing(convertEmailToDomain(user.email))
+				.then((count) => {
+					setFollowing(count);
+				})
+				.catch((error) => {
+					// Handle errors if needed
+					console.error(error);
+				});
+		}
+	}, [Followers, Following]);
+
 	const [username, setUsername] = useState('')
 	const [isCreatingChat, setIsCreatingChat] = useState(false);
 
@@ -42,13 +120,73 @@ const Message = () => {
 	}
 
 	return (
-		<ChatEngine
-			height='100vh'
-			userName={user?.displayName}
-			userSecret={user?.email}
-			projectID='ae8bcf07-cc70-40a3-9a12-3e70be74c505'
-			renderNewChatForm={(creds) => renderChatForm(creds)}
-		/>
+		<div className="h-fit w-full bg-[#0b1539]">
+			<div className="flex  justify-between bg-[#0b1539] sticky top-0 w-full h-[12vh] shadow-md shadow-black" style={{ zIndex: 50 }}>
+				<div className="text-white flex gap-8 text-xl place-items-center ps-10">
+					<button onClick={toggleDropdown}>
+						{isDropdownOpen ? <RxCross1 size={40} /> : <GiHamburgerMenu size={40} />}
+					</button>
+					{isDropdownOpen && (
+						<div className="absolute w-fit rounded-lg shadow-lg bg-[#D9D9D9] ring-1 ring-black ring-opacity-5 mt-[260px]">
+							<div className="py-2 px-4" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+								<Link href="../" className="block px-4 py-2 text-black" role="menuitem">
+									Home
+								</Link>
+								<Link href="/profile/myprofile" className="block px-4 py-2 text-black" role="menuitem">
+									My Profile
+								</Link>
+								<Link href="../upload" className="block px-4 py-2 text-black" role="menuitem">
+									Add New Project
+								</Link>
+								<Link href="../">
+									<button onClick={handleSignOut} className="block px-4 py-2 text-black" role="menuitem">
+										Sign Out
+									</button>
+								</Link>
+								<Link href="" className="block px-4 py-2 text-black" role="menuitem">
+									About Us
+								</Link>
+							</div>
+						</div>
+					)}
+					<Image
+						src={userDp}
+						alt="Current User Photo"
+						height={50}
+						width={40}
+						className="rounded-full"
+					/>
+					<div>
+						{UserName}
+						<div className="flex gap-1">
+							<span className="text-[#4f96ff] px-1">followers </span>
+							{Followers}
+							<span className="text-[#eb65dd] px-1">following </span>
+							{Following}
+						</div>
+					</div>
+				</div>
+				<div className="flex justify-between gap-4 text-white p-4 pe-10 place-items-center">
+					<Link href="/">
+						<BiHomeAlt size={30} />
+					</Link>
+				</div>
+			</div>
+			<div className="px-20 mt-3">
+				<div className="w-full overflow-hidden  bg-[#ea64dc] rounded-2xl pt-5">
+					<div className="ms-8 mb-5 text-3xl font-bold text-white">
+						Messages
+					</div>
+					<ChatEngine
+						height='73.3vh'
+						userName={user?.displayName}
+						userSecret={user?.email}
+						projectID='ae8bcf07-cc70-40a3-9a12-3e70be74c505'
+						renderNewChatForm={(creds) => renderChatForm(creds)}
+					/>
+				</div>
+			</div>
+		</div>
 	)
 }
 
